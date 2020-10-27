8e2fbe53e082f94852053db30e1f389580e2a362
Jc7j <chiangjason19@gmail.com>
2020-10-27 16:10:27 -0700

    Added home, signin, landing page

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+import LandingScreen from "./screens/Landing";
+import SignInScreen from "./screens/SignIn";
+import HomeScreen from "./screens/Home";
+
+const RootStack = createStackNavigator();
+
+const App = () => {
+  const [isAuthenticated, setIsAuthenticated] = React.useState(false);
+
+  const handleSignIn = () => {
+    // TODO implement real sign in mechanism
+
+    setIsAuthenticated(true);
+  };
+  return (
+    <NavigationContainer>
+      <RootStack.Navigator>
+        {isAuthenticated ? (
+          <RootStack.Screen name="Home" component={HomeScreen} />
+        ) : (
+          <>
+            <RootStack.Screen name="Landing" component={LandingScreen} />
+            <RootStack.Screen name="Sign In">
+              {(props) => <SignInScreen {...props} onSignIn={handleSignIn} />}
+            </RootStack.Screen>
+          </>
+        )}
+      </RootStack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ src/screens/SignIn/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { View, Text, Button, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
+
+const SignInScreen = ({ onSignIn }) => {
+  return (
+    <View style={styles.container}>
+      <Text>Public Sign In Screen</Text>
+      <Button title="Sign In" onPress={onSignIn} />
+    </View>
+  );
+};
+
+export default SignInScreen;
